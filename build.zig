a67ce18b6cd9594eb6482cf552c6dd9131f401f1
GasInfinity <me@gasinfinity.dev>
2024-11-01 18:27:30 +0100

    chore: initial commit

---
+++ build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+const zine = @import("zine");
+
+pub fn build(b: *std.Build) !void {
+    const i = 10;
+    _ = i;
+    zine.website(b, .{
+        .title = "gasinfinity",
+        .host_url = "https://gasinfinity.dev",
+        .layouts_dir_path = "layouts",
+        .content_dir_path = "content",
+        .assets_dir_path = "assets",
+        .static_assets = &.{
+            "CNAME",
+
+            // Fonts referenced by css
+            "fonts/Oxanium-VariableFont_wght.ttf",
+        },
+        .debug = true,
+    });
+}
